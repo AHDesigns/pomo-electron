799b8ef7c01e394c317d708d8708dc5ab38aaa49
Adam Hall <adamhalldesigns@gmail.com>
2022-03-07 07:28:43 +0000

    update view stories

--- client/machines/view/view.stories.tsx
+++ client/machines/view/view.stories.tsx
@@ -1,13 +1,8 @@
-import React, { FC } from 'react';
+import { Inspector } from '@client/components';
+import { Meta } from '@storybook/react/types-6-0';
 import { useMachine } from '@xstate/react';
-import { Story, Meta } from '@storybook/react/types-6-0';
-// import { Inspector } from '@client/components';
-import { inspect } from '@xstate/inspect';
+import React from 'react';
 import machine from './machine';
-
-inspect({
-  iframe: false,
-});
 
 export default {
   title: 'Machines/View',
@@ -24,7 +19,7 @@ function LoginMachine(): JSX.Element {
 export function Login(): JSX.Element {
   return (
     <>
-      {/* <Inspector /> */}
+      <Inspector />
       <LoginMachine />
     </>
   );
