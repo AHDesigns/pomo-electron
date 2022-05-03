8a62dbd4e92381fcf9f4f62147033396190218a9
Adam Hall <adamhalldesigns@gmail.com>
2022-05-03 16:19:27 +0100

    fix jest prettier config for eslint

--- jest.prettier.js
+++ jest.prettier.js
@@ -1,5 +1,6 @@
 const { commonIgnore } = require('./tooling/commonIgnore');
 
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'jest-runner-prettier',
   modulePathIgnorePatterns: commonIgnore.concat('e2e'),
