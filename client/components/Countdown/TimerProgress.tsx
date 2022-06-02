6acfa5c24f28a959c56db112ffad3b709a7bb936
Adam Hall <adamhalldesigns@gmail.com>
2022-06-02 08:28:26 +0100

    fix timer

--- client/components/Countdown/TimerProgress.tsx
+++ client/components/Countdown/TimerProgress.tsx
@@ -1,23 +1,19 @@
+import { assertUnreachable } from '@shared/asserts';
 import React from 'react';
 import { testWrap } from '@client/components';
 
-interface ITimerProgress {
+export interface ITimerProgress {
   duration: number;
   mins: number;
   seconds: number;
-  state: { matches(state: string): boolean };
+  state: 'break' | 'inactive' | 'pomo';
 }
 
+const stroke = 2;
+const radius = 50 - stroke;
+const circumference = radius * 2 * Math.PI;
+
 export function TimerProgressC({ duration, mins, seconds, state }: ITimerProgress): JSX.Element {
-  const totalDurationInSeconds = duration * 60;
-
-  const secondsExpired = 60 - seconds;
-  const minutesExpired = duration - mins;
-  const timeExpired = secondsExpired + minutesExpired;
-  const stroke = 2;
-  const radius = 50 - stroke;
-  const circumference = radius * 2 * Math.PI;
-
   return (
     <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
       <circle
@@ -29,11 +25,11 @@ export function TimerProgressC({ duration, mins, seconds, state }: ITimerProgres
         strokeWidth={stroke}
       />
       <circle
-        className="stroke-thmGood"
+        className={state === 'pomo' ? 'stroke-thmPrimary' : `stroke-thmGood`}
         style={{
           transform: 'rotate(-90deg)',
           transformOrigin: 'center',
-          transition: state.matches('initial') ? 'all 0.2s' : 'all 1s',
+          transition: state === 'inactive' ? 'all 0.2s' : 'all 1s',
           transitionTimingFunction: 'linear',
         }}
         cx={50}
@@ -42,14 +38,33 @@ export function TimerProgressC({ duration, mins, seconds, state }: ITimerProgres
         fill="none"
         strokeWidth={stroke}
         strokeDasharray={circumference}
-        strokeDashoffset={
-          state.matches('initial')
-            ? circumference
-            : circumference * (1 - timeExpired / totalDurationInSeconds)
-        }
+        strokeDashoffset={circumference * getOffset()}
       />
     </svg>
   );
+
+  /**
+   * Get Progress as a percentage.
+   * As this approaches zero, the timer will appear to fill or progress
+   * i.e offset === 1 -> bar appears empty
+   * i.e offset === 0 -> bar appears full
+   */
+  function getOffset(): number {
+    const totalDurationInSeconds = duration * 60;
+    const timeExpired = seconds + 60 * mins;
+    const expiredAsPercentage = timeExpired / totalDurationInSeconds;
+
+    switch (state) {
+      case 'inactive':
+        return 1;
+      case 'break':
+        return 1 - expiredAsPercentage;
+      case 'pomo':
+        return expiredAsPercentage;
+      default:
+        return assertUnreachable(state);
+    }
+  }
 }
 
 export const TimerProgress = testWrap(TimerProgressC, 'timer-progress');
