c0acd445ca9f2b7dc3f578b1f86606bf72b7b520
Adam Hall <adamhalldesigns@gmail.com>
2022-06-18 16:10:11 +0100

    fix update event from being added repeatedly

--- electron/services/updater/updater.ts
+++ electron/services/updater/updater.ts
@@ -3,15 +3,16 @@ import { autoUpdater } from 'electron-updater';
 
 export function checkForUpdates(logger: ILogger): void {
   autoUpdater.logger = logger;
-  tryUpdate(logger);
-}
-
-function tryUpdate(logger: ILogger): void {
-  autoUpdater.checkForUpdatesAndNotify().catch(logger.errorWithContext('checking for updates'));
 
   autoUpdater.on('update-not-available', (info) => {
     setTimeout(() => {
       tryUpdate(logger);
     }, 1000 * 60 * 5);
   });
+
+  tryUpdate(logger);
+}
+
+function tryUpdate(logger: ILogger): void {
+  autoUpdater.checkForUpdatesAndNotify().catch(logger.errorWithContext('checking for updates'));
 }
